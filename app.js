6c24ddb13a0171921f6f75f9805277d939e2e092
Dickson Tse <dtse87@gmail.com>
2013-08-19 18:14:47 -0700

    Update index view to display articles

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ var articleProvider = new ArticleProvider();
 
 app.get('/', function(req, res) {
   articleProvider.findAll(function(error, docs) {
-    res.send(docs);
+    res.render('index.jade', { title: 'Blog', articles: docs });
   });
 });
 app.get('/users', user.list);
