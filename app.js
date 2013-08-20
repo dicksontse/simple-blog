3d1ad5763b2df8c7cdc52daed682d14dd8c55383
Dickson Tse <dtse87@gmail.com>
2013-08-19 18:31:31 -0700

    Add ability to make new posts

--- app.js
+++ app.js
@@ -36,6 +36,20 @@ app.get('/', function(req, res) {
     res.render('index.jade', { title: 'Blog', articles: docs });
   });
 });
+
+app.get('/blog/new', function(req, res) {
+  res.render('blog_new.jade', { title: 'New Post'});
+});
+
+app.post('/blog/new', function(req, res) {
+  articleProvider.save({
+    title: req.param('title'),
+    body: req.param('body')
+  }, function(error, docs) {
+    res.redirect('/');
+  });
+});
+
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
